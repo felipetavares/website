4eec7989352b785aa49f40382b5e6c846603dbe1
Felipe Tavares <felipe.oltavares@gmail.com>
2012-11-26 15:49:50 -0300

    Corected menu opening

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,12 @@
-$(".fleft").click (function () {
-	if (this.open) {
-		$(this).parent().animate({"width":$(this).width()},300);
-		$(this).parent().children(".menudrop").animate({},300);
-		$(this).parent().children(".menudrop").css({"display":"none"});
-		this.open = false;
-	}
-	else {
-		var sum=$(this).width();
-		$(this).parent().children(".menudrop").each( function(){ sum += $(this).width()+4;} );
-		$(this).parent().animate({"width":sum},300);
-		$(this).parent().children(".menudrop").animate({"background":"black"},300,
+$(".menuitem").hover (
+function () {
+		var sum=$(this).children(".fleft").width();
+		$(this).children(".menudrop").each( function(){ sum += $(this).width()+4;} );
+		$(this).animate({"width":sum},300);
+		$(this).children(".menudrop").animate({"background":"black"},300,
 		function () {$(this).css({"display":"block"})});
 		this.open = true;
-	}
-})
-
-$(".menuitem").hover (function() {},
+},
 function () {
 	$(this).animate({"width":$(this).children(".fleft").width()},300);
 	$(this).children(".menudrop").animate({},300);
