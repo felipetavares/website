22a4788d2d077d62c1a408abf0fac6d70c41e0f0
Felipe Tavares <felipe.oltavares@gmail.com>
2012-11-26 13:14:25 -0300

    Menu ainda mais bonito

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,15 @@
-$(".menuitem").click (function () {
+$(".menuitem .fleft").click (function () {
 	if (this.open) {
-		$(this).animate({"width":$(this).children(".fleft").width()},300);
-		$(this).children(".menudrop").animate({},300);
-		$(this).children(".menudrop").css({"display":"none"});
+		$(this).parent().animate({"width":$(this).width()},300);
+		$(this).parent().children(".menudrop").animate({},300);
+		$(this).parent().children(".menudrop").css({"display":"none"});
 		this.open = false;
 	}
 	else {
-		var sum=$(this).children(".fleft").width();
-		$(this).children(".menudrop").each( function(){ sum += $(this).width()+4;} );
-		$(this).animate({"width":sum},300);
-		$(this).children(".menudrop").animate({"background":"black"},300,
+		var sum=$(this).width();
+		$(this).parent().children(".menudrop").each( function(){ sum += $(this).width()+4;} );
+		$(this).parent().animate({"width":sum},300);
+		$(this).parent().children(".menudrop").animate({"background":"black"},300,
 		function () {$(this).css({"display":"block"})});
 		this.open = true;
 	}
