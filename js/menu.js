d4bba57dd56ce9ce1a54b2b17f7f02f297627d69
Felipe Tavares <felipe.oltavares@gmail.com>
2012-12-01 00:02:07 -0300

    Added fixed nivo height

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
 $(".menuitem").hover (
 function () {
+	if (!this.open) {
 		var sum=$(this).children(".fleft").width();
 		$(this).children(".menudrop").each( function(){ sum += $(this).width()+4;} );
 		$(this).animate({"width":sum},300);
 		$(this).children(".menudrop").animate({"background":"black"},300,
-		function () {$(this).css({"display":"block"})});
-		this.open = true;
+		function () {$(this).css({"display":"block"});$(this).parent()[0].open = true;});
+	}
 },
 function () {
-	$(this).animate({"width":$(this).children(".fleft").width()},300);
-	$(this).children(".menudrop").animate({},300);
-	$(this).children(".menudrop").css({"display":"none"});
-	this.open = false;
+	if (this.open) {
+		$(this).animate({"width":$(this).children(".fleft").width()},300);
+		$(this).children(".menudrop").animate({},300);
+		$(this).children(".menudrop").css({"display":"none"});
+		$(this)[0].open = false;
+	}
 })
